673bf36472bf0ad57a289414418ea29b2532c53b
Mohammed Hussain <mohammedhussain0898@gmail.com>
2024-04-20 15:02:04 +0530

    react router dom updated

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div className="App">
       <Router>
       <Header/>
+      <Home/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
@@ -21,7 +22,6 @@ function App() {
           <Route path='/projects' element={<Projects/>}/>
           <Route path='/contact' element={<Contact/>}/>
         </Routes>
-        <Home/>
         <About/>
         <Skills/>
         <Projects/>
